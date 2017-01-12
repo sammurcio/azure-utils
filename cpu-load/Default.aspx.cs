c7367466a48169fed5ce111a955d15dcc30ccbef
Samuel Murcio <sam.murcio@gmail.com>
2017-01-11 23:04:55 -0600

    V1.0

---
+++ cpu-load/Default.aspx.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblHostname.Text = Environment.MachineName;
+    }
+
+    public static void CPUKill(object cpuUsage)
+    {
+        Parallel.For(0, 1, new Action<int>((int i) =>
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while (true)
+            {
+                if (watch.ElapsedMilliseconds > (int)cpuUsage)
+                {
+                    Thread.Sleep(100 - (int)cpuUsage);
+                    watch.Reset();
+                    watch.Start();
+                }
+            }
+        }));
+    }
+
+    public static void app()
+    {
+        int cpuUsage = 80;
+        int time = 10000;
+        List<Thread> threads = new List<Thread>();
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(CPUKill));
+            t.Start(cpuUsage);
+            threads.Add(t);
+        }
+        Thread.Sleep(time);
+        foreach (var t in threads)
+        {
+            t.Abort();
+        }
+    }
+
+    protected void btnMaxCPU_Click(object sender, EventArgs e)
+    {
+        var timeToRun = System.DateTime.Now.AddSeconds(int.Parse(txtTime.Text));
+        while (timeToRun > System.DateTime.Now)
+        {
+            app();
+        }
+    }
+}
